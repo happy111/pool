f2fa9309eb6e238b9afe25ffa11e22a29a323d75
Umesh <shandilyajyotiraditya@gmail.com>
2020-11-18 07:51:30 +0530

    Update Code

---
+++ margin/urls.py
@@ -0,0 +1,57 @@
+"""margin URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls.static import static
+from margin import settings
+from rest_framework_simplejwt import views as jwt_views
+
+schema_view = get_schema_view(
+    openapi.Info(title="Margin Ally API DOCS", default_version="v1"),
+    public=True,
+)
+
+urlpatterns = [
+    path("", admin.site.urls),
+    path("jet/", include("jet.urls", "jet")),
+    path("jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),
+    path(
+        "apidocs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "api/v1/token/refresh/",
+        jwt_views.TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        'api/token/', 
+        jwt_views.TokenObtainPairView.as_view(), 
+        name='token_obtain_pair'),
+
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
